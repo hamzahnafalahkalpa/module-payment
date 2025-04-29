a2761a4dba8f69268819a0c8298a2a3f19637f11
Hamzah <hamzahnafalah@gmail.com>
2025-04-29 23:27:50 +0700

    update modules

--- src/Resources/Coa/ViewCoa.php
+++ src/Resources/Coa/ViewCoa.php
@@ -18,6 +18,7 @@ class ViewCoa extends ApiResource
         $arr = [
             'id'             => $this->id,
             'name'           => $this->name,
+            'code'           => $this->code,
             'status'         => $this->status,
             'created_at'     => $this->created_at,
             'updated_at'     => $this->updated_at
