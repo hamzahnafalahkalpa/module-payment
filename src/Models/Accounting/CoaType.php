3e400a937d610955167ce8acdb71e8ca2e7e6ced
Hamzah <hamzahnafalah@gmail.com>
2025-07-12 14:52:15 +0700

    update modules

--- src/Models/Accounting/CoaType.php
+++ src/Models/Accounting/CoaType.php
@@ -7,16 +7,4 @@ use Hanafalah\ModulePayment\Models\Price\FinanceStuff;
 class CoaType extends FinanceStuff
 {
     protected $table = 'unicodes';
-
-    protected static function booted(): void{
-        parent::booted();
-        static::addGlobalScope('coa_flag',function ($query) {
-            $query->isCoaType();
-        });
-        static::creating(function ($query) {
-            $query->flag ??= 'CoaType';
-        });
-    }
-
-    public function scopeIsCoaType($builder){return $builder->where('flag','CoaType');}
 }
--- src/Models/Price/FinanceStuff.php
+++ src/Models/Price/FinanceStuff.php
@@ -12,16 +12,6 @@ class FinanceStuff extends Unicode
 {
     protected $table = 'unicodes';
 
-    protected static function booted(): void{
-        parent::booted();
-        static::addGlobalScope('flag',function($query){
-            $query->flagIn('FinanceStuff');
-        });
-        static::creating(function($query){
-            $query->flag = 'FinanceStuff';
-        });
-    }
-
     public function getViewResource(){return ViewFinanceStuff::class;}
     public function getShowResource(){return ShowFinanceStuff::class;}
 }
