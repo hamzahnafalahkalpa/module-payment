8a18a6b0cff06fdf76c9a383233abddd4fd741b4
hamzah <hamzahnafalah@gmail.com>
2025-11-02 09:04:07 +0700

    update for addendums

--- assets/database/migrations/0010_01_01_000001_create_billings_table.php
+++ assets/database/migrations/0010_01_01_000001_create_billings_table.php
@@ -41,8 +41,8 @@ return new class extends Migration
                     ->constrained()->cascadeOnUpdate()->restrictOnDelete();
                 $table->string('author_type', 50)->nullable(false);
                 $table->string('author_id', 36)->nullable(false);
-                $table->string('cashier_type', 50)->nullable(false);
-                $table->string('cashier_id', 36)->nullable(false);
+                $table->string('cashier_type', 50)->nullable(true);
+                $table->string('cashier_id', 36)->nullable(true);
                 $table->string('status', 50)->default(Status::DRAFT->value)->nullable(false);
                 $table->timestamp('reported_at')->nullable();
                 $table->json('props')->nullable();
