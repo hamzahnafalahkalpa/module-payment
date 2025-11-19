591a1b6bf7db0d4b14731830f00df7b0b4146ca0
hamzah <hamzahnafalah@gmail.com>
2025-11-19 14:40:31 +0700

    update for emr

--- src/Schemas/PaymentMethod.php
+++ src/Schemas/PaymentMethod.php
@@ -4,6 +4,7 @@ namespace Hanafalah\ModulePayment\Schemas;
 
 use Hanafalah\ModulePayment\Contracts\Data\PaymentMethodData;
 use Hanafalah\ModulePayment\Contracts\Schemas\PaymentMethod as ContractsPaymentMethod;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
 class PaymentMethod extends FinanceStuff implements ContractsPaymentMethod
@@ -11,7 +12,7 @@ class PaymentMethod extends FinanceStuff implements ContractsPaymentMethod
     protected string $__entity = 'PaymentMethod';
     public $payment_method_model;
 
-    public function prepareStorePaymentMethod(PaymentMethodData $payment_method_dto){
+    public function prepareStorePaymentMethod(PaymentMethodData $payment_method_dto): Model{
         $payment_method = $this->prepareStoreFinanceStuff($payment_method_dto);
         return $this->payment_method_model = $payment_method;
     }
